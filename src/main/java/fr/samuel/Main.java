fcb3c2eaf628a3fe8052c62b9847bd6e29db595d
Samuel-Chapuis <samuel.chapuis@hotmail.com>
2023-04-03 18:57:02 +0200

    La nouvelle branche

--- src/main/java/fr/samuel/Main.java
+++ src/main/java/fr/samuel/Main.java
@@ -4,5 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Merci Kerian !!");
+        System.out.println("Merci Kerian !!");
+        System.out.println("Merci Kerian !!");
+        System.out.println("Merci Kerian !!");
+        System.out.println("Merci Kerian !!");
+        System.out.println("Merci Kerian !!");
+
+
     }
 }
