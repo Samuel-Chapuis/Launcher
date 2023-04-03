d16aa58fcefdebcfcb9d91fed0b43fe530f058d0
Samuel-Chapuis <samuel.chapuis@hotmail.com>
2023-04-03 18:58:35 +0200

    toujours plus de test

--- src/main/java/fr/samuel/Main.java
+++ src/main/java/fr/samuel/Main.java
@@ -3,6 +3,10 @@ package fr.samuel;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
         System.out.println("Merci Kerian !!");
     }
 }
