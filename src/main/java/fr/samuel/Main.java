1aebf7ae4a9618d835573772b93994b77d30d4aa
Samuel-Chapuis <samuel.chapuis@hotmail.com>
2023-04-03 18:31:46 +0200

    on essaye des trucs avec Kerian

--- src/main/java/fr/samuel/Main.java
+++ src/main/java/fr/samuel/Main.java
@@ -1,5 +1,8 @@
-package fr.samuel;public class Main {
+package fr.samuel;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Merci Kerian !!");
     }
 }
